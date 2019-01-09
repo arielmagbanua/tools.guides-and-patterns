71f9ef8f751c10f23f34e9b88e7fb56e1b01e73d
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-01-09 08:05:23 +0800

    Added comment in homestead.bat

--- homestead/homestead.bat
+++ homestead/homestead.bat
@@ -1,6 +1,8 @@
 @echo off
 
 set cwd=%cd%
+
+:: Make sure to tweak the example C:\Homestead path in the script to the actual location of your Homestead installation.
 set homesteadVagrant=C:\Homestead
 
 cd /d %homesteadVagrant% && vagrant %*
